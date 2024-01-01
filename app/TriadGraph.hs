f3a02f11af5f916f2d7d8e86e47f3a3101e5ccb5
Jim Snavely <ludflu@gmail.com>
2023-12-31 20:29:53 -0500

    get the triads going

--- app/TriadGraph.hs
+++ app/TriadGraph.hs
@@ -1,7 +1,15 @@
 module TriadGraph where
 
+import qualified Data.Graph.Inductive as G
 import qualified Data.List as DL
+import qualified Data.Map as Map
 
+type TriadNodeLabel = [String] -- the node is the set of notes in the triad
+
+type TriadEdgeLabel = Int -- the edge is the differeint in magnitude of the tone being swapped
+-- out compared to the tone being swapped in
+
+notetriads :: [[String]]
 notetriads =
   [ ["C", "D#", "G"], --
     ["C", "D#", "G#"], --
@@ -29,7 +37,7 @@ notetriads =
     ["E", "G#", "B"]
   ]
 
-hasEdge :: [String] -> [String] -> Bool
+hasEdge :: TriadNodeLabel -> TriadNodeLabel -> Bool
 hasEdge triad1 triad2 =
   let sharedNotes = DL.intersect triad1 triad2
    in length sharedNotes == 2
@@ -37,9 +45,29 @@ hasEdge triad1 triad2 =
 --  findTriadEdges
 -- for each triad
 
-findMates :: [[String]] -> [String] -> [[String]]
+findMates :: [TriadNodeLabel] -> TriadNodeLabel -> [TriadNodeLabel]
 findMates triads triad = DL.filter (hasEdge triad) triads
+
+nodeLookup :: Map.Map TriadNodeLabel Int
+nodeLookup =
+  let nodesWithIndex = zip notetriads [0 ..]
+   in Map.fromList nodesWithIndex
 
 neighborChords =
   let cfinder = findMates notetriads
    in map cfinder notetriads
+
+makePairs :: a -> [a] -> [(a, a)]
+makePairs item friends = map (\f -> (f, item)) friends
+
+triadEdges :: [([String], [String])]
+triadEdges =
+  let es = map (\t -> (t, findMates notetriads t)) notetriads
+      pairs = concatMap (\(triad, mates) -> makePairs triad mates) es
+   in pairs
+
+-- triadGraph :: Graph gr => gr TriadNodeLabel TriadEdgeLabel
+-- triadGraph = mkGraph nodes es
+--   where
+--     nodes = zip [0 ..] notetriads
+--     es = triadEdges
