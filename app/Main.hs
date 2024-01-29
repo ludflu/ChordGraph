98ab98283ab0b83eac587865fbe00aaa89eb7b2b
Jim Snavely <ludflu@gmail.com>
2024-01-29 16:00:16 -0500

    I think its working now

--- app/Main.hs
+++ app/Main.hs
@@ -4,20 +4,17 @@ import ChordGraph
 import System.Random
   ( Random (randomR),
     RandomGen,
-    mkStdGen,
-    randomRIO,
+    getStdGen,
+    initStdGen,
   )
 
 main :: IO ()
-main = do
-  let gen = mkStdGen 321 -- 123
-      cmajor = ["C", "E", "G"]
-      randomTransforms = [pl, slide, rp, prl]
-      -- (randomTransforms, _) = fisherYates gen transforms
-      -- (randomTransformNames, _) = fisherYates gen transformNames
-      path = take 4 randomTransforms
-  -- pathNames = take 4 randomTransformNames
-  -- print pathNames
-  -- let progression = cmajor : findChordProgression cmajor [pr, pl] -- fails
-  let progression = cmajor : findChordProgression cmajor [p, r, l] -- fails
-  printFlat progression
+main =
+  do
+    gen <- initStdGen
+    let cmajor = ["C", "E", "G"]
+        (randomTransforms, _) = fisherYates gen transforms
+        (randomTransformNames, _) = fisherYates gen transformNames
+        path = take 4 randomTransforms
+        progression = cmajor : findChordProgression cmajor randomTransforms -- fails
+    printFlat progression
