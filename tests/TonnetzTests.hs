b9727dfb9e99faa01d966cbb92c3fef94c138863
Jim Snavely <ludflu@gmail.com>
2024-01-29 10:51:55 -0500

    start to test and  find the bug

--- tests/TonnetzTests.hs
+++ tests/TonnetzTests.hs
@@ -1,7 +1,22 @@
 module Main where
 
+import ChordGraph (triadGraph)
+import Data.Foldable
+import qualified Data.Graph.Inductive as G
 import Test.Tasty (TestTree, defaultMain, testGroup)
-import Test.Tasty.HUnit (assertEqual, testCase)
+import Test.Tasty.HUnit (assertBool, assertEqual, testCase)
+
+chordNodes = G.nodes triadGraph
+
+goodNode :: G.Node -> Bool
+goodNode n =
+  let es = G.out triadGraph n
+   in length es == 3
+
+allGoodNodes :: [G.Node] -> Bool
+allGoodNodes ns =
+  let ag = map goodNode ns
+   in and ag
 
 testOne = testCase "test one" $ assertEqual "test one" 3 4
 
@@ -9,7 +24,18 @@ tonnetzUnitTests :: TestTree
 tonnetzUnitTests =
   testGroup
     "tonnetz tests"
-    [testOne]
+    [ -- testOne,
+      testCase "All good nodes" $
+        assertEqual
+          "all good"
+          True
+          ( allGoodNodes
+              chordNodes
+          ),
+      testCase
+        "number of nodes"
+        $ assertEqual "num nodes" (length chordNodes) 25
+    ]
 
 main :: IO ()
 main = defaultMain tonnetzUnitTests
